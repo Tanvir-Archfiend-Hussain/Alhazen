354b11bedc5f4feba39e17567ac870d5c63965a9
archfiend-Tanvir <tanvirelhussain@gmail.com>
2023-04-30 15:48:59 +0100

    JS

--- JavaScript APi/Turorial ONe/tutOne/main.js
+++ JavaScript APi/Turorial ONe/tutOne/main.js
@@ -1,1 +1,11 @@
 const searchBX = document.querySelector('.search-box')
+searchBX.addEventListener('keypress', setQuery)
+
+function setQuery(evt) {
+    if (evt.keyCode == 13) {
+        getResults(searchBX.value)
+        console.log(searchBX.value);
+    }
+}
+
+setQuery()
