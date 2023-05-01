1a3b3ea6ad0afb9e58a9a7ccc32d8bb3d3ad5480
archfiend-Tanvir <tanvirelhussain@gmail.com>
2023-05-01 18:07:56 +0100

    JAVASCRIPT

--- JavaScript APi/Turorial ONe/tutOne/main.js
+++ JavaScript APi/Turorial ONe/tutOne/main.js
@@ -1,10 +1,19 @@
-const searchBX = document.querySelector('.search-box')
-searchBX.addEventListener('keypress', setQuery)
+/**
+ * 
+ *  
+ * const searchBX = document.querySelector('.search-box')
+ * searchBX.addEventListener('keypress', setQuery)
 
+  
 function setQuery(evt) {
     if (evt.keyCode == 13) {
         //getResults(searchBX.value)
         console.log(searchBX.value);
     }
 }
+*/
 
+let file = "annual-enterprise-survey-2021-financial-year-provisional-csv.text"
+fetch (file)
+.then(x => x.text())
+.then(y => document.getElementById("demo").innerHTML = y);
