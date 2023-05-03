820e2ebef0eca81384b2edc8c0cc726e1a6efa59
archfiend-Tanvir <tanvirelhussain@gmail.com>
2023-05-03 19:08:57 +0100

    js

--- JavaScript APi/Turorial ONe/tut3three/script.js
+++ JavaScript APi/Turorial ONe/tut3three/script.js
@@ -1,16 +1,20 @@
 // fetching LOREM paragraphs
+/** 
 let para = document.getElementById("para")
 
 fetch('readme.txt')
 .then(x => x.text())
 .then(y => para.innerHTML = y)
-
+*/
 
 // fetch images
 function pic01Func() {
-    let imagePicOne = document.getElementById('pic01')
-    fetch('https://img.freepik.com/premium-photo/young-handsome-blond-guy-bedroom-with-large-window-with-curtains-bed-naked-some-rainbow-colored-panties-lgbt-community_255766-3393.jpg')
-    .then(x => x.blob())
-    .then(y => imagePicOne.innerHTML = y)
+    fetch("https://ae01.alicdn.com/kf/HTB1HCERIFXXXXb_XVXXq6xXFXXXE/220440850/HTB1HCERIFXXXXb_XVXXq6xXFXXXE.jpg_640x640q90.jpg?size=73560&height=379&width=750&hash=2c699457e9d1dcd97f8a997a6bdf34c6")
+    .then(x => {
+        return x.blob()
+    })
+    .then(y => {
+        document.getElementById('pic01').src = URL.createObjectURL(y)
+    })
 }
 
