ffba5babcdebf41bbefff1408c8725f1fb75b9e8
archfiend-Tanvir <tanvirelhussain@gmail.com>
2023-05-03 18:51:30 +0100

    js

--- JavaScript APi/Turorial ONe/tut3three/script.js
+++ JavaScript APi/Turorial ONe/tut3three/script.js
@@ -7,3 +7,10 @@ fetch('readme.txt')
 
 
 // fetch images
+function pic01Func() {
+    let imagePicOne = document.getElementById('pic01')
+    fetch('https://img.freepik.com/premium-photo/young-handsome-blond-guy-bedroom-with-large-window-with-curtains-bed-naked-some-rainbow-colored-panties-lgbt-community_255766-3393.jpg')
+    .then(x => x.blob())
+    .then(y => imagePicOne.innerHTML = y)
+}
+
