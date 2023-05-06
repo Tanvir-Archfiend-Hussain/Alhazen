20517c236e9b71be19275789b3f7456c1d93285f
archfiend-Tanvir <tanvirelhussain@gmail.com>
2023-05-06 20:15:10 +0100

    JS

--- Tutu2343/script.js
+++ Tutu2343/script.js
@@ -1,8 +1,7 @@
-getText("")
+getText("fetch_info.txt")
 
 async function getText(file) {
     let x = await fetch(file)
     let y = await x.text()
-
     document.getElementById("demiID").innerHTML = y;
 }
