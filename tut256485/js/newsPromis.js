379ef8886833e512a4a221e5073b2aa5a3b97ecb
archfiend-Tanvir <tanvirelhussain@gmail.com>
2023-05-09 23:05:14 +0100

    JS

--- tut256485/js/newsPromis.js
+++ tut256485/js/newsPromis.js
@@ -7,5 +7,15 @@ function Get01() {
     .then(c => {
         return c.blob()
     }).then(c => {
-        document.getElementById('Id02').src = URL.createObjectURL(c)
+        document.getElementById('Id01').src = URL.createObjectURL(c)
 })}
+
+function Get02() {
+    fetch('')
+    .then(x => {
+        return x.blob()
+    })
+    .then(y => {
+        document.getElementById('Id02').src = URL.createObjectURL(y)
+    })
+}
